325d3b840aa6c8cf667230f823dcabccf76de833
ujwbukka <ujwbukka@amazon.com>
2022-02-17 13:30:30 -0500

    Updated pooled lambda execution role to remove dynamodb access and update user management service so that sys admin can get, update and delete an user

--- server/TenantManagementService/user-management.py
+++ server/TenantManagementService/user-management.py
@@ -192,7 +192,7 @@ def get_user(event, context):
         return utils.create_unauthorized_response()
     else:
         user_info = get_user_info(event, user_pool_id, user_name)
-        if(user_info.tenant_id!=tenant_id):
+        if(not auth_manager.isSystemAdmin(user_role) and user_info.tenant_id!=tenant_id):
             logger.log_with_tenant_context(event, "Request completed as unauthorized. Users in other tenants cannot be accessed")
             return utils.create_unauthorized_response()
         else:
@@ -229,7 +229,7 @@ def update_user(event, context):
         return utils.create_unauthorized_response()
     else:
         user_info = get_user_info(event, user_pool_id, user_name)
-        if(user_info.tenant_id!=tenant_id):
+        if(not auth_manager.isSystemAdmin(user_role) and user_info.tenant_id!=tenant_id):
             logger.log_with_tenant_context(event, "Request completed as unauthorized. Users in other tenants cannot be accessed")
             return utils.create_unauthorized_response()
         else:
@@ -275,7 +275,7 @@ def disable_user(event, context):
     
     if (auth_manager.isTenantAdmin(user_role) or auth_manager.isSystemAdmin(user_role)):
         user_info = get_user_info(event, user_pool_id, user_name)
-        if(user_info.tenant_id!=tenant_id):
+        if(not auth_manager.isSystemAdmin(user_role) and user_info.tenant_id!=tenant_id):
             logger.log_with_tenant_context(event, "Request completed as unauthorized. Users in other tenants cannot be accessed")
             return utils.create_unauthorized_response()
         else:
