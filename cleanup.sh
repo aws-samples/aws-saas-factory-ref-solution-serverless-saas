dfc342289ebc173d304c0eabfe579d53dde7a1ac
Syed Hussain <suhussai@ualberta.ca>
2023-05-12 13:20:52 -0600

    fix cleanup.sh merge conf

--- cleanup.sh
+++ cleanup.sh
@@ -167,7 +167,8 @@ while true; do
     if [[ "${next_token}" == "" ]]; then
         response=$( aws cognito-idp list-user-pools --max-results 1)
     else
-        response=$( aws cognito-idp list-user-pools --max-results 1 --next-token $next_token)
+        # using next-token instead of starting-token. See: https://github.com/aws/aws-cli/issues/7661
+        response=$( aws cognito-idp list-user-pools --max-results 1 --next-token "$next_token")
     fi
 
     pool_ids=$(echo "$response" | jq -r '.UserPools[] | select(.Name | test("^.*-ServerlessSaaSUserPool$")) |.Id')
