225a7869b9b003dcd1e730b3f5b32933f47a24cf
Babak Parvizi <bparvizi@amazon.com>
2024-06-10 15:06:21 -0700

    Refactored to accommodate SBT architecture.

--- server/resources/lambda-prepare-deploy.py
+++ server/resources/lambda-prepare-deploy.py
@@ -1,22 +1,17 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+import os
 import boto3
 import json
 import zipfile
-import tempfile
 import traceback
 
 print('Loading function')
 s3 = boto3.client('s3')
 code_pipeline = boto3.client('codepipeline')
 dynamodb = boto3.resource('dynamodb')
-
-# TODO: Update CDK stack to pass in table references.
-table_tenant_stack_mapping = dynamodb.Table('ServerlessSaaS-TenantStackMapping')
-# TODO: TenantDetails is now part of SBT, update CDK project to get reference from SBT control plane.
-table_tenant_details = dynamodb.Table('ServerlessSaaS-TenantDetails')
-table_tenant_settings = dynamodb.Table('ServerlessSaaS-Settings')
+table_tenant_stack_mapping = dynamodb.Table(os.environ['TENANT_MAPPING_TABLE'])
 
 def find_artifact(artifacts, name):
     """Finds the artifact 'name' among the 'artifacts'
@@ -36,37 +31,6 @@ def find_artifact(artifacts, name):
 
     raise Exception('Input artifact named "{0}" not found in event'.format(name))
 
-def get_template_url(s3, artifact, file_in_zip):
-    """Gets the template artifact
-
-    Downloads the artifact from the S3 artifact store to a temporary file
-    then extracts the zip and returns the file containing the CloudFormation
-    template.
-
-    Args:
-        artifact: The artifact to download
-        file_in_zip: The path to the file within the zip containing the template
-
-    Returns:
-        The CloudFormation template as a string
-
-    Raises:
-        Exception: Any exception thrown while downloading the artifact or unzipping it
-
-    """
-    tmp_file = tempfile.NamedTemporaryFile()
-    bucket = artifact['location']['s3Location']['bucketName']
-    print(bucket)
-
-    key = artifact['location']['s3Location']['objectKey']
-    print(key)
-    with tempfile.NamedTemporaryFile() as tmp_file:
-        s3.download_file(bucket, key, tmp_file.name)
-        with zipfile.ZipFile(tmp_file.name, 'r') as zip:
-            extracted_file = zip.extract(file_in_zip, '/tmp/')
-            s3.upload_file(extracted_file, bucket, file_in_zip)
-            template_url =''.join(['https://', bucket,'.s3.amazonaws.com/',file_in_zip])
-            return template_url
 
 def put_job_success(job, message):
     """Notify CodePipeline of a successful job
@@ -83,6 +47,7 @@ def put_job_success(job, message):
     print(message)
     code_pipeline.put_job_success_result(jobId=job)
 
+
 def put_job_failure(job, message):
     """Notify CodePipeline of a failed job
 
@@ -97,6 +62,7 @@ def put_job_failure(job, message):
     print('Putting job failure')
     print(message)
     code_pipeline.put_job_failure_result(jobId=job, failureDetails={'message': message, 'type': 'JobFailed'})
+
 
 def get_user_params(job_data):
     """Decodes the JSON user parameters and validates the required properties.
@@ -122,7 +88,6 @@ def get_user_params(job_data):
         # then fail the job with a helpful message.
         raise Exception('UserParameters could not be decoded as JSON')
 
-
     if 'artifact' not in decoded_parameters:
         # Validate that the artifact name is provided, otherwise fail the job
         # with a helpful message.
@@ -135,49 +100,13 @@ def get_user_params(job_data):
 
     return decoded_parameters
 
-def get_tenant_params(tenantId):
-    """Get tenant details to be supplied to Cloud formation
-
-    Args:
-        tenantId (str): tenantId for which details are needed
-
-    Returns:
-        params from tenant management table
-    """
-    # TODO: Check if Cognito user poole/appClientId still required.
-    if (tenantId != "pooled"):
-        tenant_details = table_tenant_details.get_item(Key={'tenantId': tenantId})
-        userPoolId = tenant_details['Item']['userPoolId']
-        appClientId = tenant_details['Item']['appClientId']
-    else:
-        tenant_details = table_tenant_settings.get_item(Key={'settingName': 'userPoolId-pooled'})
-        userPoolId = tenant_details['Item']['settingValue']
-        tenant_details = table_tenant_settings.get_item(Key={'settingName': 'appClientId-pooled'})
-        appClientId = tenant_details['Item']['settingValue']
-
-    params = []
-    param_tenantid = {}
-    param_tenantid['ParameterKey'] = 'TenantIdParameter'
-    param_tenantid['ParameterValue'] = tenantId
-    params.append(param_tenantid)
-
-    param_userpoolid = {}
-    param_userpoolid['ParameterKey'] = 'CognitoUserPoolIdParameter'
-    param_userpoolid['ParameterValue'] = userPoolId
-    params.append(param_userpoolid)
-
-    param_appclientid = {}
-    param_appclientid['ParameterKey'] = 'CognitoAppClientIdParameter'
-    param_appclientid['ParameterValue'] = appClientId
-    params.append(param_appclientid)
-
-    return params
 
 def add_parameter(params, parameter_key, parameter_value):
     parameter = {}
     parameter['ParameterKey'] = parameter_key
     parameter['ParameterValue'] = parameter_value
     params.append(parameter)
+
 
 def lambda_handler(event, context):
     """The Lambda function handler
@@ -197,41 +126,23 @@ def lambda_handler(event, context):
 
         # Extract the params
         params = get_user_params(job_data)
-        artifact = params['artifact']
-        template_file = params['template_file']
         commit_id = params['commit_id']
 
-
         # Get the list of artifacts passed to the function
-        input_artifacts = job_data['inputArtifacts']
         output_artifact = job_data['outputArtifacts'][0]
-
 
         # Get all the stacks for each tenant to be updated/created from tenant stack mapping table
         mappings = table_tenant_stack_mapping.scan()
-
+        print('mappings success: ', mappings)
         output_bucket = output_artifact['location']['s3Location']['bucketName']
         output_key = output_artifact['location']['s3Location']['objectKey']
         stacks = []
 
-        #Create array to pass to step function
+        # Create array to pass to step function
         for mapping in mappings['Items']:
             stack = mapping['stackName']
             tenantId = mapping['tenantId']
             waveNumber = mapping['waveNumber']
-
-            # TODO: Check if parameters are still required for the new deployment.
-            # Get the parameters to be passed to the Cloudformation from tenant table
-            #params = get_tenant_params(tenantId)
-            # Passing parameter to enable canary deployment for lambda's
-            #add_parameter(params, 'LambdaCanaryDeploymentPreference', "True")
-
-            # Get the artifact details
-            #artifact_data = find_artifact(input_artifacts, artifact)
-
-            # Get the JSON template file out of the artifact
-            #template_url = get_template_url(s3, artifact_data, template_file)
-
             stacks.append(
                 {
                     "stackName": stack,
@@ -239,7 +150,6 @@ def lambda_handler(event, context):
                     "commitId": commit_id,
                     "waveNumber": int(waveNumber)
                 })
-        print("mappings success......")
 
     except Exception as e:
         # If any other exceptions which we didn't expect are raised
@@ -249,17 +159,17 @@ def lambda_handler(event, context):
         traceback.print_exc()
         put_job_failure(job_id, 'Function exception: ' + str(e))
 
-    print (stacks)
+    print(stacks)
 
-    #write stacks variable to file
+    # write stacks variable to file
     with open('/tmp/output.json', 'w') as outfile:
         json.dump({"stacks": stacks}, outfile)
 
-    #zip the file
+    # zip the file
     with zipfile.ZipFile('/tmp/output.json.zip', 'w') as zip:
         zip.write('/tmp/output.json', 'output.json')
 
-    #upload the output to output_bucket in s3
+    # upload the output to output_bucket in s3
     s3.upload_file('/tmp/output.json.zip', output_bucket, output_key)
     print('output.json.zip uploaded to s3')
 
