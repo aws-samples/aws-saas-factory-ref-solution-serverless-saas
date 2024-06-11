f8882dc0ae6531f8ac79102e689f8d4a75934641
Babak Parvizi <bparvizi@amazon.com>
2024-06-11 13:51:03 -0700

    Set placeholder email.

--- scripts/update-tenant.sh
+++ scripts/update-tenant.sh
@@ -12,11 +12,9 @@ export CDK_PARAM_CODE_COMMIT_REPOSITORY_NAME="aws-saas-factory-ref-solution-serv
 export CDK_PARAM_LAMBDA_CANARY_DEPLOYMENT_PREFERENCE="true"
 export CDK_PARAM_COMMIT_ID=$CODE_COMMIT_ID
 echo "CDK_PARAM_COMMIT_ID: $CDK_PARAM_COMMIT_ID"
-
-# TODO: Not required for tenantTemplateStack. Test before removing.
-export CDK_PARAM_SYSTEM_ADMIN_EMAIL="$EMAIL",
+export CDK_PARAM_SYSTEM_ADMIN_EMAIL="EMAIL"
 
 cd server/
 npm install
 
-npx cdk deploy --stacks $STACK_NAME --all --require-approval never
+npx cdk deploy $STACK_NAME --require-approval never
