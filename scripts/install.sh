981d9229c85c31c0c6bab6b76904160338c4707c
Babak Parvizi <bparvizi@amazon.com>
2024-07-24 12:23:38 -0700

    Removed uncommented code.

--- scripts/install.sh
+++ scripts/install.sh
@@ -24,13 +24,13 @@ export CDK_PARAM_COMMIT_ID=$(git log --format="%H" -n 1)
 
 # Preprovision pooled infrastructure
 cd ../server/cdk
-#npm install
+npm install
 
-#npx -y cdk bootstrap
-#npx -y cdk deploy --all --require-approval never --concurrency 10 --asset-parallelism true
+npx -y cdk bootstrap
+npx -y cdk deploy --all --require-approval never --concurrency 10 --asset-parallelism true
 
-#echo "Installing client applications, please wait...."
-#sleep 60
+echo "Installing client applications, please wait...."
+sleep 60
 
 # Deploy client UIs
 export CDK_PARAM_REG_API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name ControlPlaneStack --query "Stacks[0].Outputs[?OutputKey=='controlPlaneAPIEndpoint'].OutputValue" --output text)
